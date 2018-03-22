3ab6e0eb04702e93042004050c340503528d33ad
Xiao <371975976@qq.com>
2018-03-22 14:31:49 +0800

    add Unicode parse

--- leptjson.c
+++ leptjson.c
@@ -105,7 +105,46 @@ static int lept_parse_number(lept_context *c, lept_value *v) {
     return LEPT_PARSE_OK;
 }
 
+static const char * lept_parse_hex4(const char *p, unsigned *u) {
+    // unsigned 4位
+    // int 16位
+    *u = 0;
+    for (int i=0; i<4; ++i) {
+        char ch = *p++;
+        *u <<= 4;
+        if(ch >= '0' && ch <= '9') *u |= ch - '0';
+        else if(ch >= 'A' && ch <= 'F') *u |= ch -('A' - 10);
+        else if(ch >= 'a' && ch <= 'f') *u |= ch -('a' - 10);
+        else return NULL;
+    }
+    return p;
+}
+static void lept_encode_utf8(lept_context* c, unsigned u) {
+    // 按照UTF-8码点会映射到1-4个字节
+    if (u <= 0x7F) 
+        PUTC(c, u & 0xFF);
+    else if (u <= 0x7FF) {
+        PUTC(c, 0xC0 | ((u >> 6) & 0xFF));
+        PUTC(c, 0x80 | ( u       & 0x3F));
+    }
+    else if (u <= 0xFFFF) {
+        PUTC(c, 0xE0 | ((u >> 12) & 0xFF));
+        PUTC(c, 0x80 | ((u >>  6) & 0x3F));
+        PUTC(c, 0x80 | ( u        & 0x3F));
+    }
+    else {
+        assert(u <= 0x10FFFF);
+        PUTC(c, 0xF0 | ((u >> 18) & 0xFF));
+        PUTC(c, 0x80 | ((u >> 12) & 0x3F));
+        PUTC(c, 0x80 | ((u >>  6) & 0x3F));
+        PUTC(c, 0x80 | ( u        & 0x3F));
+    }
+}
+
+#define STRING_ERROR(ret) do { c->top = head; return ret; } while(0)
+
 static int lept_parse_string(lept_context *c, lept_value *v) {
+    unsigned u, u2;     // 用于解析Unicode字符.
     size_t head = c->top, len;      // 备份栈顶，用于计算字符串长度
     const char *p;
     EXPECT(c, '\"');
@@ -128,19 +167,31 @@ static int lept_parse_string(lept_context *c, lept_value *v) {
                     case 'n':  PUTC(c, '\n'); break;
                     case 'r':  PUTC(c, '\r'); break;
                     case 't':  PUTC(c, '\t'); break;
+                    case 'u':
+                            if (!(p = lept_parse_hex4(p, &u)))
+                                STRING_ERROR(LEPT_PARSE_INVALID_UNICODE_HEX);
+                            if (u >= 0xD800 && u <= 0xDBFF) { /* surrogate pair代理对方式 */
+                                if (*p++ != '\\')
+                                    STRING_ERROR(LEPT_PARSE_INVALID_UNICODE_SURROGATE);
+                                if (*p++ != 'u')
+                                    STRING_ERROR(LEPT_PARSE_INVALID_UNICODE_SURROGATE);
+                                if (!(p = lept_parse_hex4(p, &u2)))
+                                    STRING_ERROR(LEPT_PARSE_INVALID_UNICODE_HEX);
+                                if (u2 < 0xDC00 || u2 > 0xDFFF)
+                                    STRING_ERROR(LEPT_PARSE_INVALID_UNICODE_SURROGATE);
+                                u = (((u - 0xD800) << 10) | (u2 - 0xDC00)) + 0x10000;
+                            }
+                            lept_encode_utf8(c, u);
+                            break;
                     default:
-                        c->top = head;
-                        return LEPT_PARSE_INVALID_STRING_ESCAPE;
+                        STRING_ERROR(LEPT_PARSE_INVALID_STRING_ESCAPE);
                 }
                 break;
             case '\0':
-                c->top = head;
-                return LEPT_PARSE_MISS_QUOTATION_MARK;
+                STRING_ERROR(LEPT_PARSE_MISS_QUOTATION_MARK);
             default:
-                if((unsigned char) ch < 0x20) {     // 无效字符
-                    c->top = head;
-                    return LEPT_PARSE_INVALID_STRING_CHAR;
-                }
+                if((unsigned char) ch < 0x20)     // 无效字符
+                    STRING_ERROR(LEPT_PARSE_INVALID_STRING_CHAR);
                 PUTC(c, ch);
         }
     }
